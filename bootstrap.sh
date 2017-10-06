67a45c180bc62a3e4d310cd560b6f896bf64cd8d
advancevillage <cugriver@gmail.com>
2017-10-06 15:31:53 +0800

    0.1.0

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,13 +27,24 @@ deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable
 EOF
 ) > /etc/apt/sources.list.d/docker.list
 
-
 #创建应用用户/组 cuger
-sudo groupadd cuger
-sudo useradd -c cuger -d /home/cuger -g cuger -s /sbin/nologin  cuger
+webuser=cuger
+webpasswd=richardkelly
+sudo groupadd $webuser
+sudo useradd -c $webuser -d /home/$webuser -g $webuser -s /bin/bash  $webuser
+echo $webuser:$webpasswd | sudo chpasswd
+sudo bash -c "echo \"$webuser ALL=(ALL) NOPASSWD:ALL\" > /etc/sudoers.d/$webuser"
+
+if [ ! -d /home/$webuser ]; then
+    sudo mkdir -p /home/$webuser
+    sudo chown -R $webuser:$webuser /home/$webuser
+fi
 
 #修改系统时区时间 Asia/Shanghai
 sudo timedatectl set-timezone Asia/Shanghai
+
+#修改hostname
+sudo hostnamectl set-hostname localhost
 
 #修改系统终端显示颜色配置
 (
@@ -54,24 +65,23 @@ sudo apt-get -y install tmux
 sudo apt-get -y install git
 sudo apt-get -y install git-flow
 sudo apt-get -y install nmon
+sudo apt-get -y install aptitude
 
 (
 cat << EOF
 set encoding=utf-8
 set termencoding=utf-8
-set fileencodings=utf-8,ucs-bom,cp936,gbk,gck2312,gb18030
+set fileencodings=utf-8
 set number
 set ruler
 set mouse=a
 set nocompatible
 set novisualbell
 set cmdheight=2
-set wildmenu
 set backspace=indent,eol,start
 set syntax=on
 set t_Co=256
 set syntax=enable
-set shortmess=atI
 set cul
 set cuc
 set go=
@@ -80,11 +90,7 @@ set showmode
 set magic
 set history=500
 set report=0
-filetype on
-filetype plugin on
-set confirm
 set clipboard=unnamed
-filetype indent on
 set viminfo+=!
 set nobackup
 set noswapfile
@@ -95,22 +101,15 @@ set shiftwidth=4
 set expandtab
 set iskeyword+=_,@,$,%,#,-
 set whichwrap+=<,>,h,l
-set formatoptions=tcrqn
 set autoindent
-set nowrap
-set whichwrap=b,s,<,>,[,]
 set showmatch
-set matchtime=1
-set incsearch 
 EOF
-) > /etc/vim/vimrc
+) >> /etc/vim/vimrc
 
-
-
-
-
-
-
-
-
-
+#设置同步文件夹
+if [ ! -d /opt/web ]; then
+    sudo mkdir -p /opt/web
+    sudo chown -R $webuser:$webuser /opt/web
+else
+    sudo chown -R $webuser:$webuser /opt/web
+fi
